6473f747ab7e903ec3e5fd6ecaa4e0a5a789571d
cOOci <109303543+cOOciDev@users.noreply.github.com>
2025-09-23 18:40:41 +0330

    update configs

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // allow external access on VPS
+    port: 5174,        // your custom port
+    strictPort: true
+  }
 })
