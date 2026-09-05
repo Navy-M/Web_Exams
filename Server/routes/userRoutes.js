32e7873be889634738667d2b1c55cc5ae3ff4380
cOOci <109303543+cOOciDev@users.noreply.github.com>
2026-09-05 10:52:11 +0330

    Update userRoutes.js

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -10,7 +10,7 @@ import {
 
 const router = express.Router();
 
-router.delete("/:id", protect, deleteUser);
+router.delete("/:id", protect, admin, deleteUser);
 router.post("/completeProf", protect, completeProfile); // Authenticated user's own profile
 router.get("/profile", protect, getProfile); // Authenticated user's own profile
 router.get("/", protect, admin, getUsers); // Admin: list all users
